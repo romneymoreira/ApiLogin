d645ee8ecfab8c2ac8458f26615d705e81fdc758
romneymoreira <romneymoreira@gmail.com>
2017-09-10 14:28:01 -0300

    Primeiro commit

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Http;
+using log4net;
+using PDev.Auth.Api.Attributes;
+
+namespace PDev.Auth.Api
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+           config.Filters.Add(new ErrorHandlerAttribute(LogManager.GetLogger(typeof(ErrorHandlerAttribute).FullName)));
+        }
+    }
+}
